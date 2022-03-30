1a1d1a17a8939029444492eb4e9b9b9a8df9b6ba
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-03-30 10:51:01 +0800

    add-new-demo-code

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,31 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [Time, setTime] = useState(ini);
+  // "Your code here";
 
   const isStart = "Your code here";
   const active = "Your code here";
-  const refInterval = "Your code here";
+  const refInterval = useRef(null);
 
   const startTimer = () => {
-    "Your code here";
+    // "Your code here";
+    refInterval.current = setInterval(() => {
+      setTime((timer) => timer + 1)
+    }, 1000)
     active.current.disabled = true;
   };
   const stopTimer = () => {
-    "Your code here";
+    // "Your code here";
+    clearInterval(refInterval.current)
   };
   const resetTimer = () => {
-    "Your code here";
+    // "Your code here";
+    clearInterval(refInterval.current)
     active.current.disabled = false;
+
   };
 
-  return { time, startTimer, stopTimer, resetTimer, active };
+  return { Time, startTimer, stopTimer, resetTimer, active };
 };
 export default useTimer;
